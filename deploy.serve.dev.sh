d53aca168f4870fb39e5d8de0371bf057ad3ff61
leekangtaqi <leekangtaqi@sina.com>
2016-07-29 16:16:55 +0800

    update deploy shell

--- deploy.serve.dev.sh
+++ deploy.serve.dev.sh
@@ -1,8 +1,3 @@
 cd /root/git/songni/songli_serve
-git checkout dev
-git pull origin dev
-
-npm install
-
-cd /root/git/songni/songli_deploy
-pm2 reload serve-dev
+export NODE_ENV=development
+grunt serve
--- deploy.serve.prd.sh
+++ deploy.serve.prd.sh
@@ -3,6 +3,7 @@ git checkout master
 git pull origin master
 
 npm install
+bower install --allow-root
 
 cd /root/git/songni/songli_deploy
 pm2 reload serve
